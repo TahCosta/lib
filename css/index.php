3b30e09adc47a7d1912878e57a23548a04815dfc
TahCosta <tdesouzacosta@gmail.com>
2016-04-04 09:26:36 -0300

    Lib

---
+++ css/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Utf-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="Style.css">
+<title>Biblioteca Css</title>
+</head>
+    <body>
+    <div class="container-fluid">
+    
+    </div>
+    </body>
+  
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Utf-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="Style.css">
+<title>Biblioteca de Linguagens</title>
+</head>
+    <body>
+	<div class="container-fluid">
+	<div class="jumbotron">
+	<h3 class="text-center">Progresso de construção da Biblioteca de Linguagens</h3>
+		<div class="row">
+	 <div class="col-md-offset-2 col-md-2"><a class="spritehtml" href="html/index.php"></a></div>
+	<div class="col-md-2"><a class="spritecss" href="css/index.php"></a></div>
+	<div class="col-md-2"><a class="spritejs" href="javascript/index.php"></a></div>
+	<div class="col-md-2"><a class="spritephp" href="php/index.php"></a></div>
+</div>
+<div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="10"
+  aria-valuemin="0" aria-valuemax="400" style="width:10%">
+    <span>10% Completo</span>
+</div>
+</div>
+</div>
+</div>
+    </body>
+  
+</html>
